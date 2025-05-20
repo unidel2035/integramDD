2d7df7d91c5080699fc52f5415ba7b9c18546b32
horhe-dvlp <horhe-dvlp@yandex.ru>
2025-05-20 23:56:14 +0300

    fix: Enhance deployment script to wait for PostgreSQL readiness and improve error handling

--- .sql_to_load/upload_methods.sh
+++ .sql_to_load/upload_methods.sh
@@ -1,3 +1,19 @@
-export PGPASSWORD='postgres';
-psql -h localhost -U postgres -d integram -f ./objs2.sql
-psql -h localhost -U postgres -d integram -f ./terms6.sql
+#!/bin/bash
+
+while getopts u:p:d: flag; do
+  case "${flag}" in
+    u) DB_USER=${OPTARG} ;;
+    p) DB_PASS=${OPTARG} ;;
+    d) DB_NAME=${OPTARG} ;;
+  esac
+done
+
+if [[ -z "$DB_USER" || -z "$DB_PASS" || -z "$DB_NAME" ]]; then
+  echo "Usage: $0 -u <user> -p <password> -d <database>"
+  exit 1
+fi
+
+export PGPASSWORD="$DB_PASS"
+
+psql -h localhost -U "$DB_USER" -d "$DB_NAME" -f ./objs2.sql
+psql -h localhost -U "$DB_USER" -d "$DB_NAME" -f ./terms6.sql

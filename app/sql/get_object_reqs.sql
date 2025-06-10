c3420e31e1b5562859019d139bc9698d7ca69a2f
horhe-dvlp <horhe-dvlp@yandex.ru>
2025-06-10 21:49:17 +0300

    refactor: Update SQL query to fetch request values and types

--- app/sql/get_object_reqs.sql
+++ app/sql/get_object_reqs.sql
@@ -1,4 +1,3 @@
-SELECT obj.id AS object_id, vals.t AS req_t, vals.val AS value
-FROM {db} obj
-JOIN {db} vals ON vals.up = obj.id
-WHERE obj.t = {term_id} AND obj.up = {parent_id};
+SELECT reqs.val req_val, typs.id req_t, typs.val refr, '' arr_num
+  FROM {db} reqs JOIN {db} typs ON typs.id=reqs.t
+  WHERE reqs.up={term_id};
